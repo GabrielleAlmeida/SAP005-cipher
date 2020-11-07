8df543e26a9783b285b2c9e626146b4fd1b3a50c
Gabrielle Almeida <gabi.antunes.almeida@gmail.com>
2020-11-07 15:16:17 -0300

    Aplicação web feita utilizando a Cifra de César

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,37 @@
+
+
+ const alphabetReference = 26;
+ const firstLetterCode = 65;
+
+
+//ENCONDE
+function cipherResultEncode (cipherText, offset){
+  let cipher = ""
+  for(let counter = 0; counter < cipherText.length; counter++){
+      const charCodeLetter = cipherText.charCodeAt(counter) 
+      const resultOfTheCodingFormula= ((charCodeLetter - firstLetterCode + offset) % alphabetReference ) + firstLetterCode
+      cipher = cipher + String.fromCharCode(resultOfTheCodingFormula) 
+  }
+
+  return cipher       
+}
+
+//DECODE
+function cipherResultDecode (text, offsetDecode){
+  let cipher = ""
+  for(let counter = 0; counter < text.length; counter++){
+      const charCodeLetter = text.charCodeAt(counter) 
+      const resultOfTheDecodingFormula = ((charCodeLetter - firstLetterCode +(alphabetReference - (offsetDecode % alphabetReference))) % alphabetReference) + firstLetterCode
+      cipher = cipher + String.fromCharCode(resultOfTheDecodingFormula)
+  }
+
+  return cipher 
+}
+
 const cipher = {
-  // ...
+  encode: cipherResultEncode,
+  decode: cipherResultDecode,
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//ENCONDE
+function cipherEncode(){
+    const textareaEncodeValue = document.getElementById("story-encode").value.toUpperCase()
+    if (textareaEncodeValue === "") {
+        alert ('Preencha o campo com o texto para cifrar');
+    } 
+    else {   
+        const keyEncodeValue = parseInt(document.getElementById("key-encode").value)
+        if (!keyEncodeValue){
+            alert ('Especifique o número da chave');   
+        }   
+        else {
+            const resultCoding = cipher.encode(textareaEncodeValue, keyEncodeValue)
+            
+            const textareaEncodeDestinyElement = document.getElementById('story-encode-result')
+            textareaEncodeDestinyElement.innerHTML = resultCoding
+        }
+    }
+}
+const startButtonEncode = document.getElementById("button-encode")
+startButtonEncode.addEventListener("click",cipherEncode)
+
+// DECODE
+
+function cipherDecode(){
+    const textareaDecodeValue = document.getElementById("story-decode").value.toUpperCase()
+    if (textareaDecodeValue === "") {
+        alert ('Preencha o campo com o texto Cifrado');
+    } 
+    else {   
+        const keyDecodeValue = parseInt(document.getElementById("key-decode").value)
+        if (!keyDecodeValue){
+            alert ('Especifique o número da chave');   
+        }   
+        else {
+            const resultDecoding = cipher.decode(textareaDecodeValue, keyDecodeValue)
+
+            const textareaDecodeDestinyElement = document.getElementById('story-decode-result')
+            textareaDecodeDestinyElement.innerHTML = resultDecoding
+        }
+    }
+}
+const startButtonDecode= document.getElementById("button-decode")
+startButtonDecode.addEventListener("click",cipherDecode) 
+
+
